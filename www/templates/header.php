1c4e9f294e3520e7fd8f1e7c48541405c1e45e3d
Alexander Chaplygin <chaplygin82@yandex.ru>
2015-09-10 16:26:47 +0300

    Завел проект

---
+++ www/templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ru">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Європейський молодіжний фонд</title>
+
+		<base href="http://<? echo $_SERVER['HTTP_HOST'] ?>" />
+
+		<link type="text/css" href="css/reset.css" rel="stylesheet"  media="screen" />
+		<link type="text/css" href="css/layout.css" rel="stylesheet"  media="screen" />
+		<link type="text/css" href="css/ui.css" rel="stylesheet"  media="screen" />
+		<link type="text/css" href="css/header.css" rel="stylesheet"  media="screen" />
+		<link type="text/css" href="css/footer.css" rel="stylesheet"  media="screen" />
+		<link type="text/css" href="css/page-main.css" rel="stylesheet"  media="screen" />
+	</head>
+	<body>
